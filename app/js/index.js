b724bf58c80ad3e429e7e599b3072e545f5e9b40
mustafa <mustafa.bhm3@gmail.com>
2022-10-25 16:17:36 -0600

    adding humberger menue transition

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,13 @@
-console.log("js its working ");
+console.log("js is working ");
+
+/// to add and remove humberger menu class
+const btnHamburger = document.querySelector("#btnHumburger");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("clickeeed");
+  if (btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.remove("open");
+  } else {
+    btnHamburger.classList.add("open");
+  }
+});
