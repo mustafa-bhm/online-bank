9957b09d6157225782e2568e0a7d676c2a5c01aa
mustafa <mustafa.bhm3@gmail.com>
2022-10-26 11:51:28 -0600

    adding animation for humberger menu

--- app/js/index.js
+++ app/js/index.js
@@ -2,12 +2,20 @@ console.log("js is working ");
 
 /// to add and remove humberger menu class
 const btnHamburger = document.querySelector("#btnHumburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 
 btnHamburger.addEventListener("click", function () {
   console.log("clickeeed");
-  if (btnHamburger.classList.contains("open")) {
-    btnHamburger.classList.remove("open");
+  if (header.classList.contains("open")) {
+    // to close menu
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
   } else {
-    btnHamburger.classList.add("open");
+    // to open menu
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
   }
 });
