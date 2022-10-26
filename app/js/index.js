1f299b6424f7401bafd69aaab2fb7f96edf9faf8
mustafa <mustafa.bhm3@gmail.com>
2022-10-26 12:38:17 -0600

    adding animation to mobile menu

--- app/js/index.js
+++ app/js/index.js
@@ -4,18 +4,23 @@ console.log("js is working ");
 const btnHamburger = document.querySelector("#btnHumburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElms = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", function () {
   console.log("clickeeed");
   if (header.classList.contains("open")) {
     // to close menu
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeElms.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
     // to open menu
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    fadeElms.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
 });
