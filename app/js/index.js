b80770f3da4a186c00458a2dba0996d6d06616e7
mustafa <mustafa.bhm3@gmail.com>
2022-10-26 12:50:06 -0600

    adding no-scroll when menu is displayed

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,7 @@
 console.log("js is working ");
 
 /// to add and remove humberger menu class
+const body = document.querySelector("body");
 const btnHamburger = document.querySelector("#btnHumburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
@@ -10,6 +11,7 @@ btnHamburger.addEventListener("click", function () {
   console.log("clickeeed");
   if (header.classList.contains("open")) {
     // to close menu
+    body.classList.remove("noscroll");
     header.classList.remove("open");
     fadeElms.forEach(function (element) {
       element.classList.remove("fade-in");
@@ -17,6 +19,7 @@ btnHamburger.addEventListener("click", function () {
     });
   } else {
     // to open menu
+    body.classList.add("noscroll");
     header.classList.add("open");
     fadeElms.forEach(function (element) {
       element.classList.remove("fade-out");
